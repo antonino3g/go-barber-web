1a6e5d1216ed1d585023ac54ed2877bc3251e836
Antonino Praxedes <antonino.praxedes@gmail.com>
2020-07-04 22:36:06 -0400

    adding error input styles

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -22,7 +22,9 @@ const SignUp: React.FC = () => {
 
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string().required('E-mail obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
 
